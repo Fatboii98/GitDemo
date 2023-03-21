b1448e6ccaa5f927d6ecaa5dd6dbcfdaec03a7ef
“Fatboii98” <yeoweihan@u.nus.edu>
2023-03-21 21:14:34 +0800

    included lost files lol

---
+++ RetailCoreBankingSystem/TellerTerminalClient/src/java/tellerterminalclient/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tellerterminalclient;
+
+import ejb.session.stateless.CustomerSessionBeanRemote;
+import entity.Customer;
+import java.util.List;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author yeowh
+ */
+public class Main {
+
+    @EJB
+    private static CustomerSessionBeanRemote customerSessionBeanRemote;
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        List<Customer> customers = customerSessionBeanRemote.retrieveAllCustomers();
+        
+        customers.forEach(customer -> {
+            System.out.println("customerID" + customer.getCustomerId() + "; customer name = " + customer.getCustomerName());
+        });
+        
+        
+    }
+    
+}
