28f9da2b137bde49c2c877f760a9a92f4515ff8c
“Fatboii98” <yeoweihan@u.nus.edu>
2023-03-21 21:20:24 +0800

    included lost files lol

---
+++ RetailCoreBankingSystem/RetailCoreBankingSystem-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.singleton;
+
+import ejb.session.stateless.CustomerSessionBeanLocal;
+import entity.Customer;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author yeowh
+ */
+@Singleton
+@LocalBean
+@Startup
+
+public class DataInitSessionBean {
+
+    @EJB
+    private CustomerSessionBeanLocal customerSessionBeanLocal;
+
+    @PersistenceContext(unitName = "RetailCoreBankingSystem-ejbPU")
+    private EntityManager em;
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    @PostConstruct
+    public void postConstruct() {
+        if(em.find(Customer.class, 1l) == null){
+            customerSessionBeanLocal.createNewCustomer(new Customer("Record A"));
+            customerSessionBeanLocal.createNewCustomer(new Customer("Record B"));
+            customerSessionBeanLocal.createNewCustomer(new Customer("Record C"));
+            customerSessionBeanLocal.createNewCustomer(new Customer("Record D"));
+            customerSessionBeanLocal.createNewCustomer(new Customer("Record E"));
+        }
+    } 
+}
